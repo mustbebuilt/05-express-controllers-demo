1e4874c2e1c8f3b745dd768548e0a3aa36c74081
martin@mustbebuilt.co.uk <martinmustbebuilt@gmail.com>
2022-08-30 10:33:04 +0100

    Set up for Tutorial

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 05 Express Controllers Demo
 
-Demo with routes and controllers to show modularization.
+Demo with routes and controllers to show modularization and the use of functionality such as Promises.
+
+Code snippets available in the (Wiki)[https://github.com/mustbebuilt/05-express-controllers-demo/wiki/Code-snippets-for-the-controller-controller.js-file]
--- routes/routes.js
+++ routes/routes.js
@@ -2,26 +2,9 @@ const express = require("express");
 
 const router = express.Router();
 
+const fs = require("fs");
+
 // require controllers
-
-const myControllers = require("../controllers/controllers.js");
-
-var dogData = myControllers.getDogs();
-
-// using a Promise
-// var staffDataPromise = myControllers.getStaff();
-// var staffData = [];
-// staffDataPromise.then(function(data) {
-//     staffData = data;
-//   });
-
-// using async/await
-var staffData = [];
-var staffDataPromise = async () => {
-  staffData = await myControllers.getStaff();
-};
-staffDataPromise();
-// console.dir(staffData);
 
 router.get("/example", (req, res) => {
   return res.render("example", {
@@ -36,24 +19,6 @@ router.get("/main", (req, res) => {
     message: "Hello Template built in parts",
     showMsg: true,
     headingOne: "Page made from parts",
-  });
-});
-
-// dogs route
-
-// router.get("/dogs", (req, res) => {
-//   return res.render("dogs-loop", {
-//     title: "Looping Data with EJS",
-//     dogs: dogData,
-//   });
-// });
-
-// staff route
-
-router.get("/staff", (req, res) => {
-  return res.render("staff-loop", {
-    title: "Staff Data",
-    staffList: staffData,
   });
 });
 
